d085d4592790fd504bffbf0232a8b699d95eaffe
Jorge Hinojosa <jorgexhinojosa@gmail.com>
2019-03-21 12:32:11 -0500

    Fixed JS 3 project

--- javascript-3/student.js
+++ javascript-3/student.js
@@ -14,7 +14,7 @@ const yourName = 'Jorge';
 //////////////////STEP 3////////////////////
 //Create a function called 'setGroceryListTitle' that takes in one parameter called x. The function should return a string of 'XPARAM's Grocery List'.
 
-let setGroceryListTitle = ((x) => x + "'s Grocery List")
+const setGroceryListTitle = ((x) => x + "'s Grocery List")
 
 //////////////////STEP 4////////////////////
 //Create a function called 'addItem' that takes in one paramter called item. Add the item param to the end of the groceries array created above. Inside of addItem, invoke displayData() which is a function we created that takes care of some of the leg work unrelated to this module.
@@ -36,10 +36,17 @@ function removeItem(index) {
 //Create a function called 'checkGroceryCount'. Inside of the function check the total number of groceries you need to get using .length. Using an else/if statement, check if the number is greater than or equal to 5, return the string 'That looks like a big trip' from the function. If the number of groceries is equal to one, return the string '1 item'. For numbers between 1 and 5, return the string 'NUMBER items' with number replacing the number of items
 
 function checkGroceryCount() {
-    let total = groceries.length;
-    if (total >= 5) {return 'That looks like a big trip!'}
-    else if (total = 1) {return '1 item'}
-    else {return total + ' items'}
+    let count = groceries.length;
+    if (count >= 5) {
+        return 'That looks like a big trip';
+    }
+    else if (count === 1) {
+        return '1 item';
+    }
+    else if (1 < count && count < 5) {
+        return count + ' items';
+    }
+    else return 'No items in cart';
 }
 //////////////////STEP 6////////////////////
 // Refresh the index.html page. You should now see the same thing you saw when you first loaded the page but you should now be able to add items to your grocery list with a dynamic total showing. If you double click on an item, it should remove that item
